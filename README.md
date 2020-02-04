b4ad9e657ccf8b954f4a203e51af854e17be4b8a
Szymon Wiszczuk <golota60@gmail.com>
2020-02-04 21:51:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Things to change before deploying:
+
+    1.index.html -> title
+    2.change the favicon.ico
+    
+    
+    
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            bruh
+        </div>
+    );
+}
+
+export default App;
