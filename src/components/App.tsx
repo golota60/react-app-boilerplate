30d51dc82c01f1bcedd45c6146b3de21f0dd71d2
Szymon Wiszczuk <golota60@gmail.com>
2020-02-04 23:27:21 +0100

    Added Typescript

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
     return (
@@ -7,5 +8,4 @@ const App = () => {
         </div>
     );
 }
-
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js', //entry for the application
+    mode: "production",
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
     output: {
         path: path.join(__dirname, '/dist'),        //puts one big js here
         filename: 'index_bundle.js'
@@ -15,8 +20,27 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
             }
         ]
+    },
+    externals: {
+        //we want to load react externally to shorten the load times
+        "react": "React",
+        "react-dom": "ReactDOM"
     },
     plugins: [
         new HtmlWebpackPlugin({
